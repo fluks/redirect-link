14190e5f8dd9b7efac97b4d4786a5c75984fcf08
fluks <fluks.github@gmail.com>
2017-08-23 17:38:03 +0300

    Create context menus when the addon is enabled
    Create context menus when the addon is enabled.
    
    Add the title to url mapping when the addon is installed.

--- background/background.js
+++ background/background.js
@@ -1,14 +1,33 @@
 'use strict';
 
-let rows = null;
+let rows = {};
+
+/**
+ */
+const loadRows = () => {
+    browser.storage.local.get(null).then((options) => {
+        Object.keys(options).forEach(title => {
+            const opt = options[title];
+            if (opt.enabled) {
+                browser.contextMenus.create({
+                    id: title,
+                    contexts: [ 'link' ],
+                    title: title,
+                });
+                rows[title] = {
+                    url: opt.url,
+                };
+            }
+        });
+    });
+};
 
 /**
  * @param changes {}
  */
 const updateContextMenus = (changes) => {
-    const remove = browser.contextMenus.removeAll();
-    rows = {};
-    remove.then(() => {
+    browser.contextMenus.removeAll().then(() => {
+        rows = {};
         Object.keys(changes).forEach(title => {
             const options = changes[title].newValue;
 
@@ -30,12 +49,19 @@ const updateContextMenus = (changes) => {
  * @param details {}
  */
 const setDefaultRows = (details) => {
-    browser.storage.local.set({
-        'Google WebCache': {
-            enabled: true,
-            url: 'https://webcache.googleusercontent.com/search?q=cache:', 
-        },
-    });
+    const title = 'Google WebCache';
+    const url = 'https://webcache.googleusercontent.com/search?q=cache:';
+
+    rows[title] = {
+        url: url,
+    };
+
+    const opt = {};
+    opt[title] = {
+        enabled: true,
+        url: url,
+    };
+    browser.storage.local.set(opt);
 };
 
 /**
@@ -52,7 +78,8 @@ const openTab = (info, tab) => {
     browser.tabs.create({ url: url, index: tab.index + 1 });
 };
 
-browser.storage.onChanged.addListener(updateContextMenus);
-browser.runtime.onInstalled.addListener(setDefaultRows);
-browser.contextMenus.onClicked.addListener(openTab);
+loadRows();
 
+browser.runtime.onInstalled.addListener(setDefaultRows);
+browser.storage.onChanged.addListener(updateContextMenus);
+browser.contextMenus.onClicked.addListener(openTab);
