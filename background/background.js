e8575b7dbc39ac2aa418ecb1e83ebffe5a35d92b
fluks <fluks.github@gmail.com>
2018-12-26 14:15:00 +0200

    Add individual path index and query parameter formats
    Add format for path index, i.e. you can use only a part of the path in
    your redirect url.
    
    Add format for individual query parameter, i.e. you can use a single key
    from query parameters in your redirect url.

--- background/background.js
+++ background/background.js
@@ -106,6 +106,42 @@ const setDefaultOptions = (details) => {
 };
 
 /**
+ * Replace a path format string.
+ * @param format {String} Path format string.
+ * @param paths {Array[String]} All the path parts in an array.
+ * @param path {String} Whole path in one string.
+ * @return {String} The replacement string for the path format.
+ * @throw {String} If path format has an index that is greater or equal than
+ * the path has path parts.
+ */
+const replacePath = (format, paths, path) => {
+    if (format === '%p')
+        return path;
+    const m = format.match(/\[(\d+)\]/);
+    if (m[1] >= paths.length)
+        throw 'Path index out of bounds';
+    return paths[m[1]];
+};
+
+/**
+ * Replace a query parametformat string.
+ * @param format {String} Query parameter format string.
+ * @param params {Object[String]} All the query parameters in an object.
+ * @param param {String} All the query parameters in one string.
+ * @return {String} The replacement string for the query parameter format.
+ * @throw {String} If query parameter format has a key that doesn't exist in
+ * query parameters.
+ */
+const replaceParam = (format, params, param) => {
+    if (format === '%q')
+        return param;
+    const m = format.match(/\[([^\]]+)\]/);
+    if (!params.hasOwnProperty(m[1]))
+        throw 'Non-existing query parameter';
+    return params[m[1]];
+};
+
+/**
  * Replace the formats in the redirect URL with parts from the link's URL. If
  * the redirect URL doesn't contain any format, the link's URL is appended to
  * the redirect URL.
@@ -113,32 +149,46 @@ const setDefaultOptions = (details) => {
  * %u - entire URL
  * %s - scheme
  * %h - hostname
- * %p - path
- * %q - query parameters
+ * %p - path. Or %p[N], where N is index of the path part. e.g. in
+ * http://example.com/a/b/c?param=1, %p[0] is a, %p[1] is b and %p[2] is c.
+ * %q - query parameters. Or %q[KEY], where KEY is the name of the query
+ * parameter. e.g. in http://example.com/?a=1&b=2 %q[a] is 1 and %q[b] is 2.
  * %f - fragment
  * @param linkUrl {String} Link's URL.
  * @return {String} URL with formats replaced.
+ * @throw {String} If path index is out of bounds or if query parameter
+ * doesn't exist.
  */
 const replaceFormats = (url, linkUrl) => {
     const a = document.createElement('a');
     a.href = linkUrl;
 
     if (!/%(s|h|p|q|f|u)/.test(url))
-        url += linkUrl;
-    else {
-        url = url.replace(new RegExp('%s', 'g'), a.protocol.replace(':', ''));
-        url = url.replace(new RegExp('%h', 'g'), a.hostname);
-        url = url.replace(new RegExp('%p', 'g'), a.pathname);
-        url = url.replace(new RegExp('%q', 'g'), a.search.replace('?', ''));
-        url = url.replace(new RegExp('%f', 'g'), a.hash);
-        url = url.replace(new RegExp('%u', 'g'), linkUrl);
-    }
+        return url + linkUrl;
+
+    const paths = a.pathname.split('/').filter(p => p);
+    url = url.replace(/%p(\[\d+\])?/g, (s) => replacePath(s, paths, a.pathname));
+
+    const search = a.search.replace('?', '');
+    const params = {};
+    search.split('&').forEach(p => {
+        const [ key, value ] = p.split('=');
+        params[key] = value;
+    });
+    url = url.replace(/%q(\[[^\]]+\])?/g, (s) => replaceParam(s, params, search));
+
+    const scheme = a.protocol.replace(':', '');
+    url = url.replace(new RegExp('%s', 'g'), scheme);
+    url = url.replace(new RegExp('%h', 'g'), a.hostname);
+    url = url.replace(new RegExp('%f', 'g'), a.hash);
+    url = url.replace(new RegExp('%u', 'g'), linkUrl);
 
     return url;
 };
 
 /**
- * Redirect the current tab or link to new tab.
+ * Redirect the current tab or link to new tab. If replaceFormats throws an
+ * error, redirection doesn't happen.
  * @param info {OnClickData} Information about the item clicked and the context
  * where the click happened.
  * @param tab {chrome.tabs.Tab} The details of the tab where the click took
@@ -160,7 +210,13 @@ const redirect = (info, tab) => {
         }
 
         let redirectUrl = options.rows[menuItemId].url;
-        redirectUrl = replaceFormats(redirectUrl, targetUrl);
+        try {
+            redirectUrl = replaceFormats(redirectUrl, targetUrl);
+        }
+        catch (error) {
+            console.log(error);
+            return;
+        }
 
         chrome.tabs.create({
             url: redirectUrl, index: tab.index + 1,
@@ -172,6 +228,8 @@ const redirect = (info, tab) => {
 /**
  * Hide redirects which have an enableURL setting set and doesn't match the
  * link's or page's URL.
+ * <all_urls> permission is needed for many of info's properties, @see
+ * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/menus/onShown.
  * @param info {Object} OnClickData and contexts and menuIds.
  * @param tab {tabs.Tab} Details of the tab where the menu is shown.
  */
